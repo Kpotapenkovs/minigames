9121cfcdb8309b426db071eaf2fce6c22d525562
Aleksis622 <ipb23.a.eglitis@vtdt.edu.lv>
2025-10-30 09:41:19 +0200

    uzsaku route

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,7 @@
+<nav>
+<ul>
+    <li><a href="/">Sākums</a></li>
+     <li><a href="/memorycard">Kāršu atmiņu spēle</a></li>
+    <li><a href="/typingspeed">Rakstīšanas ātruma spēle</a></li>
+</ul>
+</nav>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+
+<x-slot:title>
+Homepage
+</x-slot:title>
+
+
+
+
+
+
+
+
+
+
+
+</x-layout>
